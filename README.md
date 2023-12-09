30b93dcbb784adc4a24205e56d85b5d826785b34
Venkata Mahesh Polumuri <venkat2552004@gmail.com>
2023-12-09 17:01:08 +0530

    Create all files

---
+++ README.md
@@ -0,0 +1,7 @@
+git init - create a new local repo (current folder)
+npm init -y   -> setup node environment
+npm install mongodb - installs mongodb node_modules
+git add * - adds all modified files to check
+
+git fetch origin - drops all your local changes and commits, fetch the latest history from the server
+
---
+++ app.js
@@ -0,0 +1,29 @@
+const { MongoClient } = require('mongodb')
+const uri = require('./atlas_uri')
+
+const client = new MongoClient(uri)
+const dbname = "node_practice"
+
+const connectToDatabase = async () => {
+    try {
+        await client.connect()
+        console.log(`Successfully connected to database named ${dbname}`)
+    }
+    catch (err) {
+        console.log(`Error connecting to database : ${err}`)
+    }
+};
+
+const main = async () => {
+    try {
+        await connectToDatabase();
+    }
+    catch (err) {
+        console.log(`Error connecting to database : ${err}`)
+    }
+    finally {
+        await client.close()
+    }
+};
+
+main()
